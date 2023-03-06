b9eb8bc1c22eea3571fff2daa05a7abadddbe8d0
Alex Pitzel <pitzelalex@gmail.com>
2023-03-06 16:00:19 -0500

    test: Create recipes by random country test

--- spec/requests/api/v1/recepies_request_spec.rb
+++ spec/requests/api/v1/recepies_request_spec.rb
@@ -19,5 +19,23 @@ describe 'Recipes API' do
       expect(recipes[:data][0][:attributes][:country]).to be_a String
       expect(recipes[:data][0][:attributes][:image]).to be_a String
     end
+
+    it 'by random country if country not specified' do
+      get '/api/v1/recipes'
+
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_an Array
+      expect(recipes[:data][0][:id]).to be nil
+      expect(recipes[:data][0][:type]).to eq('recipe')
+      expect(recipes[:data][0][:attributes]).to be_a Hash
+      expect(recipes[:data][0][:attributes][:title]).to be_a String
+      expect(recipes[:data][0][:attributes][:url]).to be_a String
+      expect(recipes[:data][0][:attributes][:country]).to be_a String
+      expect(recipes[:data][0][:attributes][:image]).to be_a String
+    end
   end
 end
