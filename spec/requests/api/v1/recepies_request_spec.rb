0b1b9895a534e940663ae27ef3f0db991fb199c9
Alex Pitzel <pitzelalex@gmail.com>
2023-03-06 16:25:08 -0500

    test: Create edge case testing for empty string country or no results

--- spec/requests/api/v1/recepies_request_spec.rb
+++ spec/requests/api/v1/recepies_request_spec.rb
@@ -37,5 +37,29 @@ describe 'Recipes API' do
       expect(recipes[:data][0][:attributes][:country]).to be_a String
       expect(recipes[:data][0][:attributes][:image]).to be_a String
     end
+
+    it 'returns an empty array if country is empty string', :vcr do
+      get '/api/v1/recipes?country='
+
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_an Array
+      expect(recipes[:data].length).to eq(0)
+    end
+
+    it 'returns an empty array if there are no results', :vcr do
+      get '/api/v1/recipes?country=supercalafragalisticexpialadocious'
+
+      expect(response).to be_successful
+
+      recipes = JSON.parse(response.body, symbolize_names: true)
+
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_an Array
+      expect(recipes[:data].length).to eq(0)
+    end
   end
 end
