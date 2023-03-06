8652a87083956f0db42876a47c8472a8d50aec31
Alex Pitzel <pitzelalex@gmail.com>
2023-03-05 19:17:57 -0500

    config: Add gems and gem configs

--- spec/requests/api/v1/recepies_request_spec.rb
+++ spec/requests/api/v1/recepies_request_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-describe 'Recepeis API' do
-  describe 'sends a list of recepies' do
+describe 'Recipes API' do
+  describe 'sends a list of recipes' do
     it 'by country' do
-      get '/api/v1/recepeis?country=thailand'
+      get '/api/v1/recipes?country=thailand'
 
       expect(response).to be_successful
 
-      recepies = JSON.parse(response.body, symbolize_names: true)
+      recipes = JSON.parse(response.body, symbolize_names: true)
 
-      expect(recepies).to have_key(:data)
-      expect(recepies[:data]).to be_an Array
-      expect(recepies[:data][0][:id]).to be null
-      expect(recepies[:data][0][:type]).to eq('recipe')
-      expect(recepies[:data][0][:attributes]).to be_a Hash
-      expect(recepies[:data][0][:attributes][:title]).to be_a String
-      expect(recepies[:data][0][:attributes][:url]).to be_a String
-      expect(recepies[:data][0][:attributes][:country]).to be_a String
-      expect(recepies[:data][0][:attributes][:image]).to be_a String
+      expect(recipes).to have_key(:data)
+      expect(recipes[:data]).to be_an Array
+      expect(recipes[:data][0][:id]).to be null
+      expect(recipes[:data][0][:type]).to eq('recipe')
+      expect(recipes[:data][0][:attributes]).to be_a Hash
+      expect(recipes[:data][0][:attributes][:title]).to be_a String
+      expect(recipes[:data][0][:attributes][:url]).to be_a String
+      expect(recipes[:data][0][:attributes][:country]).to be_a String
+      expect(recipes[:data][0][:attributes][:image]).to be_a String
     end
   end
 end
