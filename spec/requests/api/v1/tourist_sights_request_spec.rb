086758872c2945b513cbd0a4b460168e87a5ace7
Alex Pitzel <pitzelalex@gmail.com>
2023-03-06 12:25:03 -0500

    fix: Fix broken nil id expectation for tourist_sights request

--- spec/requests/api/v1/tourist_sights_request_spec.rb
+++ spec/requests/api/v1/tourist_sights_request_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'tourist_sights API' do
   describe 'sends a list of tourist_sights' do
-    it 'by radious from country capital' do
+    it 'by radious from country capital', :vcr do
       get '/api/v1/tourist_sights?country=Canada'
 
       expect(response).to be_successful
@@ -11,7 +11,7 @@ describe 'tourist_sights API' do
 
       expect(sights).to have_key(:data)
       expect(sights[:data]).to be_an Array
-      expect(sights[:data][0][:id]).to be_a nil
+      expect(sights[:data][0][:id]).to be nil
       expect(sights[:data][0][:type]).to eq('tourist_sight')
       expect(sights[:data][0][:attributes]).to be_a Hash
       expect(sights[:data][0][:attributes][:name]).to be_a String
