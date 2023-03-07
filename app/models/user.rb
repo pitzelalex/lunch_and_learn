cdf3ceeb6e9b35c892169d20a25ecd9848cbe89e
Alex Pitzel <pitzelalex@gmail.com>
2023-03-07 15:56:46 -0500

    feat: Create user has many favorites relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates_presence_of :email, :name
   validates_uniqueness_of :email
+  has_many :favorites
 
   def self.generate_key
     SecureRandom.urlsafe_base64
